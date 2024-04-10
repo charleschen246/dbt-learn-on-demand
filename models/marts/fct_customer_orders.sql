d12f86e90031fb7f361c06af70348f8ed1c81c79
chengchen246@gmail.com <chengchen246@gmail.com>
2024-04-10 03:32:12 +0000

    adding testing doc

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -14,13 +14,13 @@ select p.*,
         row_number() over (order by p.order_id) as transaction_seq,
         row_number() over (partition by customer_id order by p.order_id) as customer_sales_seq,
         case when c.first_order_date = p.order_placed_at
-        then 'new'
-        else 'return' end as nvsr,
+            then 'new'
+            else 'return' end as nvsr,
         sum(total_amount_paid) over (partition by customer_id order by p.order_id asc)customer_lifetime_value,
         first_order_date as fdos
     from {{ ref('order_details') }}  p
     left join {{ ref('customer_order_details') }} as c using (customer_id)
-    order by order_id)
+order by order_id)
 
 select *
 from final
